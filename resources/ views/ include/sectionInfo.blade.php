06643aaffb57e1b381b091581862fede6d931d53
clotildo <46998677+clotildo@users.noreply.github.com>
2019-08-11 22:19:13 +0000

    Update and rename resources/ views/ include/sectionInfo.blade.php to  resources/ views/ include/sectionInfo.blade.php

--- resources/ views/ include/sectionInfo.blade.php
+++ resources/ views/ include/sectionInfo.blade.php
@@ -3,45 +3,48 @@
 		<div class="row row-bottom-padded-md">
 			<div class="col-md-12 col-md-offset-3">
 
-				<h2 class="fh5co-lead animate-box">Ici vous trouverez des objets d'accasions, nouveaux à moindre prix.</h2>
-				<p class="fh5co-sub-lead animate-box">Choisissez et rentrer en contact avec le propriétaire de l'article </p>
-
+				<h2 class="fh5co-lead animate-box" style="text-align:center; padding-right:50%;">Ici vous trouverez la solution à votre problème.</h2>
 			</div>
 		</div>
 		<div class="row">
 			
 			<div class="col-md-4 col-sm-6 col-xxs-12 animate-box">
 				<a href="images/img_1.jpg" class="fh5co-project-item image-popup">
-					<img src="images/img_1.jpg" alt="Image" class="img-responsive">
+					<img src="images/im_1.jpg" alt="Image" class="img-responsive">
 					<div class="fh5co-text">
 
-						<h2 class="fh5co-lead animate-box">Nom de l'article</h2>
-						<p class="fh5co-sub-lead animate-box">Description </p>
+						<h2 class="fh5co-lead animate-box">Abobo cité résidentielle Concorde</h2>
+						<p class="fh5co-sub-lead animate-box">à louer Villa la basse, 3chambres et1 salon, 2douches, 2wc, cour avant,cour arrière. Garage 3 vehicules. </p>
+                        <p>contact: +225 84 96 46 84</p>
 
 					</div>
 				</a>
 			</div>
 
 			<div class="col-md-4 col-sm-6 col-xxs-12 animate-box">
-				<a href="images/img_2.jpg" class="fh5co-project-item image-popup">
-					<img src="images/img_2.jpg" alt="Image" class="img-responsive">
+				<a href="images/img_.jpg" class="fh5co-project-item image-popup">
+					<img src="images/img_.jpg" alt="Image" class="img-responsive">
 					<div class="fh5co-text">
 
-						<h2 class="fh5co-lead animate-box">Nom de l'article</h2>
-						<p class="fh5co-sub-lead animate-box">Description </p>
+						<h2 class="fh5co-lead animate-box">BELLE VILLA DE 4PIECES A ABOBO CITE CONCORD</h2>
+						<p class="fh5co-sub-lead animate-box">Abobo Cité Concord villa de 4 pièces avec cour arrière avant nouvelle construction loyer 150.000fx5</p>
+                        <p>contact: +225 68 74 11 38</p>
 
 					</div>
 				</a>
 			</div>
 
 			<div class="col-md-4 col-sm-6 col-xxs-12 animate-box">
-				<a href="images/img_3.jpg" class="fh5co-project-item image-popup">
-					<img src="images/img_3.jpg" alt="Image" class="img-responsive">
+				<a href="images/img_5.jpg" class="fh5co-project-item image-popup">
+					<img src="images/img_5.jpg" alt="Image" class="img-responsive">
 					<div class="fh5co-text">
 
-						<h2 class="fh5co-lead animate-box">Nom de l'article</h2>
-						<p class="fh5co-sub-lead animate-box">Description </p>
-
+						<h2 class="fh5co-lead animate-box">
+Location villas 6 pièces - Riviera Golf 4
+</h2>
+						<p class="fh5co-sub-lead animate-box">Cocody, Abidjan Villa duplex de 6 pièces en location à la Riviera golf 4, Beverly Hills.
+Loyer : 3,8 millions à revoir</p>
+<p>contact: +225 03 46 94 81</p>
 					</div>
 				</a>
 			</div>
