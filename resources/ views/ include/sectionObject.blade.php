cb53e830ca7fb3a19c3669fe93a71af2c320f946
clotildo <46998677+clotildo@users.noreply.github.com>
2019-08-11 22:26:53 +0000

    Update and rename resources/ views/ include/sectionObject.blade.php to  resources/ views/ include/sectionObject.blade.php

--- resources/ views/ include/sectionObject.blade.php
+++ resources/ views/ include/sectionObject.blade.php
@@ -5,29 +5,36 @@
 			<div class="col-md-4 col-sm-6 col-xs-12 animate-box">
 				<div class="fh5co-feature">
 					<div class="fh5co-icon">
-						<i class="icon-paragraph"></i>
+						<i><img src="images/ter3.jpg" alt="Image"></i>
 					</div>
-					<h3>Beautiful Typography</h3>
-					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right .</p>
+					<h3>vente de beaux terrains plat de 500m2 en bordure de bitume a songon avec approbation
+</h3>
+					<p>Terrains & parcelles, Yopougon 2 000 000 FCFA
+<br>Aujourd'hui, 19:20
+ </p>
 				</div>
 			</div>
 			<div class="clearfix visible-sm-block"></div>
 			<div class="col-md-4 col-sm-6 col-xs-12 animate-box">
 				<div class="fh5co-feature">
 					<div class="fh5co-icon">
-						<i class="icon-umbrella"></i>
+						<i><img src="images/ter2.jpg" alt="Image"></i>
 					</div>
-					<h3>Eco Friendly</h3>
-					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right .</p>
+					<h3>Terrain en vente 2800m2 à grand bassam</h3>
+					<p>Terrains & parcelles, Grand-Bassam 50 000 FCFA
+<br>Aujourd'hui, 19:00</p>
 				</div>
 			</div>
 			<div class="col-md-4 col-sm-6 col-xs-12 animate-box">
 				<div class="fh5co-feature">
 					<div class="fh5co-icon">
-						<i class="icon-toggle"></i>
+						<i><img src="images/ter1.jpg" alt="Image"></i>
 					</div>
-					<h3>Wide and Boxed</h3>
-					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right .</p>
+					<h3>
+ Cocody Danga Nestlé Vente terrain 1044m2 - Côte d’Ivoire
+</h3>
+					<p>Terrains & parcelles, Cocody 500 000 000 FCFA
+<br>Aujourd'hui, 17:50</p>
 				</div>
 			</div>
 		</div>
