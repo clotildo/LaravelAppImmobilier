c46326bd4ca44413873432cbdea57abff26ffedb
clotildo <46998677+clotildo@users.noreply.github.com>
2019-08-11 22:06:07 +0000

    Update and rename resources/ views/index.blade.php to  resources/ views/index.blade.php

--- resources/ views/index.blade.php
+++ resources/ views/index.blade.php
@@ -103,15 +103,12 @@
 
 		<section id="fh5co-subscribe">
 			<div class="container">
-		
-				<h3 class="animate-box"><label for="email">Subscribe to our newsletter</label></h3>
-				<form action="#" method="post" class="animate-box">
-					<h3 class="animate-box"><label for="email">Abonnez vous à notre page</label></h3>
+		<h3 class="animate-box"><label for="email">Abonnez vous à notre page</label></h3>
 				<form action="#" method="post" class="animate-box">
 					<i class="fh5co-icon icon-paper-plane"></i>
 					<input type="email" class="form-control" placeholder="Entrez votre email" id="email" name="email">
 					<input type="submit" value="Envoyer" class="btn btn-primary">
- 				</form>
+                    </form>
 
 			</div>
 		</section>
