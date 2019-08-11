bbdffd5e1ea96f0bee89d4283470a6acc5a5cf9c
clotildo <46998677+clotildo@users.noreply.github.com>
2019-08-11 22:35:10 +0000

    Update and rename resources/views/include/sectionHeader.blade.php to  resources/ views/ include/sectionHeader.blade.php

---
+++ resources/ views/ include/sectionHeader.blade.php
@@ -0,0 +1,16 @@
+<section id="fh5co-header">
+	<div class="container">
+		<nav role="navigation">
+			<ul class="pull-left left-menu">
+				<li><a href="#">Acceuil</a></li>
+				<li><a href="#">A vendre</a></li>
+				<li><a href="#">A louer</a></li>
+			</ul>
+			<h1 id="fh5co-logo"><a href="#">Guide<br>Immobilier<span>.</span></a></h1>
+			<ul class="pull-right right-menu">
+				<li><a href="#">connexion</a></li>
+				<li class=""><a href="#">S'inscrire</a></li>
+			</ul>
+		</nav>
+	</div>
+</section>
